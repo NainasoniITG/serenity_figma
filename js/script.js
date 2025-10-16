faa5647e6d66a5071d7eeb098a746aabe894f674
naina <nainasoni@itgeeks.com>
2025-10-16 19:16:28 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -3,3 +3,60 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
+//  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+
+var swiper = new Swiper(".categories-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        750: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    },
+});
+
+var swiper = new Swiper(".featured-collection-slider", {
+    slidesPerView: 1, // default mobile
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        750: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    },
+});
+
